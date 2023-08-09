8443c2304d8b9dfde587d45aba96ea6d3bf17ca2
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-08-09 18:54:34 -0300

    transfer

---
+++ core/transfer.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from account.models import Account
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib import messages
+
+@login_required
+def search_users_account_number(request):
+    #account = Account.objects.filter(account_status="active")
+    account = Account.objects.all()
+    query = request.POST.get('account_number')
+
+    if query:
+        account = account.filter(
+            Q(account_number=query)| 
+            Q(account_id=query)
+        ).distinct()
+
+    context = {
+        'account': account,
+        'query': query,
+    }
+    return render(request, 'transfer/search-user-by-account-number.html', context)
+
+
+
+def amountTransfer(request, account_number):
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except:
+        messages.warning(request, "Account does not exist")
+        return redirect('search-account')
+    context = {
+        'account': account,
+    }
+    return render(request, 'transfer/amount-transfer.html', context)
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from core import views
+from core import transfer
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # Transfers
+    path('search-account/', transfer.search_users_account_number, name="search-account"),
+    path('amount-transfer/<account_number>/', transfer.amountTransfer, name="amount-transfer"),
+
+    # Request Money
+
+    # Add Debit Card
 ]
